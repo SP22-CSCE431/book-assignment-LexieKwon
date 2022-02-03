384f965498131c2902fed15517c36bb31804cd68
LexieKwon <kwonlexie@gmail.com>
2022-02-03 16:44:13 -0600

    Invalid unit tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Book, type: :model do
     described_class.new(title: 'harry potter')
     described_class.new(author: 'JK Rowling')
     described_class.new(price: 25)
-    described_class.new(price: 2022-02-03)
+    described_class.new(date: 2022-02-03)
   end
 
   it 'is valid with valid attributes' do
@@ -15,6 +15,21 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.date = nil
     expect(subject).not_to be_valid
   end
 end
